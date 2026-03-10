588c3e528315aba786d8a6c0968c8ba877eaab6e
cinescope-wkr <cinescope-wkr@users.noreply.github.com>
2026-03-10 08:36:02 +0000

    deploy: 10ca54c94aee11c0c46ec61f5444134ab0ccfd62

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+g171a662',
+    VERSION: '0.0.post1.dev2+g10ca54c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
